3dd058bf48af9f831601f50757b4b029d75cd7c9
NhanMai <mvtnhan@gmail.cpm>
2020-08-12 11:36:14 +0700

    Updates

---
+++ precache-manifest.04f4d7983d6a1836273b4fa6f3482b8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a72dfeaf53ff545529b73de0acf252",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6dd928622167a8635a0",
+    "url": "./static/css/2.03923625.chunk.css"
+  },
+  {
+    "revision": "b6dd928622167a8635a0",
+    "url": "./static/js/2.c6b17c7d.chunk.js"
+  },
+  {
+    "revision": "43d72dab81fe6139b99e36ca2e0cb538",
+    "url": "./static/js/2.c6b17c7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53f2250ce0e1facb306a",
+    "url": "./static/js/main.39b98fa5.chunk.js"
+  },
+  {
+    "revision": "65e3e5e238beb68eb073",
+    "url": "./static/js/runtime-main.90841789.js"
+  }
+]);
